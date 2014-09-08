7838840ce906f8659bbf9604d337c149338475e2
Spike Nox <spike01@gmail.com>
2014-09-08 12:16:37 +0100

    First test, with wall of warnings

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'sinatra'
+
+group :development, :test do
+  gem "rspec"
+  end
---
+++ lib/server.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'data_mapper'
+
+env = ENV["RACK_ENV"] || "development"
+
+
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+
+require './lib/link'
+
+DataMapper.finalize
+
+DataMapper.auto_upgrade!
