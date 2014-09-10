a7154995c53bcf4acc17e3fa898d0d7feae415bd
Spike Nox <spike01@gmail.com>
2014-09-10 14:31:00 +0100

    Added uniqueness validation

--- app/server.rb
+++ app/server.rb
@@ -46,7 +46,7 @@ use Rack::Flash
       session[:user_id] = @user.id
       redirect to('/')
     else
-      flash[:notice] = "Sorry, your passwords don't match"
+      flash.now[:errors] = @user.errors.full_messages
       erb :"users/new"
     end
   end
