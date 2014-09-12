da17def7a3441dee5c2cf214e5b905c910043bae
Spike Nox <spike01@gmail.com>
2014-09-12 18:12:45 +0100

    Fixes flash problem

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -9,7 +9,7 @@
       session[:user_id] = user.id
       redirect to '/'
     else
-      flash[:errors] = ["The email or password is incorrect"]
+      flash.now[:errors] = ["The email or password is incorrect"]
       erb :"sessions/new"
     end
   end
